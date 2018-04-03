0ca1ab3d041bdf3c33b45ebfc69278d0b77d48e1
Vincent Liu <liuyu-china@hotmail.com>
2018-04-03 17:14:42 +1000

    complete simple move function(there are still some bugs need to fix)

--- Game/src/controller/Driver.java
+++ Game/src/controller/Driver.java
@@ -3,24 +3,24 @@ package controller;
 import model.Role;
 
 public class Driver {
-	static GameInitial game = new GameInitial();
-
-	static Role hero;
-
-	Detector tileDetector = new TileDetector(getSelectedHero(),game.getTileArray());
-
-
-	public Role getSelectedHero(){
-		for(int i = 0;  i < game.getHeroArray().length; i++){
-			if(game.getHeroArray()[i].isSelected()){
-				return hero = game.getHeroArray()[i];
-			}
-		}
-		return null;
-	}
-
-
-	public static GameInitial getGame(){
-		return game;
-	}
+//	static GameInitial game = new GameInitial();
+//
+//	static Role hero;
+//
+//	Detector tileDetector = new TileDetector(getSelectedHero(),game.getTileArray());
+//
+//
+//	public Role getSelectedHero(){
+//		for(int i = 0;  i < game.getHeroArray().length; i++){
+//			if(game.getHeroArray()[i].isSelected()){
+//				return hero = game.getHeroArray()[i];
+//			}
+//		}
+//		return null;
+//	}
+//
+//
+//	public static GameInitial getGame(){
+//		return game;
+//	}
 }
